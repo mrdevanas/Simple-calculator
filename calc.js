7c610f7e3ffa86f6e9201978112eaa981c78541a
mrdevanas <52702295+mrdevanas@users.noreply.github.com>
2024-11-02 19:49:06 +0100

    calc.js

--- calc.js
+++ calc.js
@@ -1,9 +1,9 @@
-function num(number){
+    function num(number){
     document.getElementById("inp").value += number;
-    }
-    function tos() {
-        document.getElementById("inp").value = eval(document.getElementById("inp").value);
-    }
-    function again(){
-        document.getElementById("inp").value = ""
-    }
+}
+function tos(){
+document.getElementById("inp").value = eval(document.getElementById("inp").value);
+}
+function remo(){
+document.getElementById("inp").value = " ";
+}
